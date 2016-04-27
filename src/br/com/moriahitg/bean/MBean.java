e2a3d3c30a08a40e96585a94a0d9ae033bae268f
Neemias Carvalho <neemias.carvalho.silva@gmail.com>
2016-04-27 16:32:16 -0300

    Login, Menubar (Home e Sair), CSS.
    Adição da tela de login, menubar com funcionalidade de voltar para o home (falta implementar o logout/sair), posicionamento dos elementos das telas com css.

--- src/br/com/moriahitg/bean/MBean.java
+++ src/br/com/moriahitg/bean/MBean.java
@@ -23,16 +23,19 @@ public class MBean implements Serializable {
 	List<SZA990> list = new ArrayList<SZA990>();
 	SA1990DAO sa1dao = new SA1990DAO();
 	SA1990 sa1 = new SA1990();
-	String A1_COD;
+	String A1_COD, resultado;
+
+	public String redirectRecursos() {
+		return "/recursos.xhtml?faces-redirect=true";
+	}
 
 	public String logar() {
-		
-		String resultado = "";
+		resultado = "";
 		sa1 = sa1dao.getCliente(A1_COD);
 		if (sa1.getA1_COD().equals(A1_COD)) {
 			resultado = "/recursos.xhtml?faces-redirect=true";
-		} else {
-			// resultado
+		} else if (sa1.getA1_COD().equals("nada")) {
+			resultado = "Login inválido";
 		}
 		return resultado;
 	}
@@ -58,5 +61,13 @@ public class MBean implements Serializable {
 
 	public void setA1_COD(String a1_COD) {
 		A1_COD = a1_COD;
+	}
+
+	public String getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(String resultado) {
+		this.resultado = resultado;
 	}
 }
--- src/br/com/moriahitg/dao/SA1990DAO.java
+++ src/br/com/moriahitg/dao/SA1990DAO.java
@@ -1,7 +1,5 @@
 package br.com.moriahitg.dao;
 
-import java.util.Iterator;
-
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -10,15 +8,16 @@ import br.com.moriahitg.modelo.SA1990;
 
 public class SA1990DAO {
 	
-	@SuppressWarnings("rawtypes")
 	public SA1990 getCliente(String A1_COD) {
 		SA1990 sa1 = new SA1990();
+		sa1.setA1_COD("nada");
 		try {
 			Session session = HibernateUtil.getSessionFactory().openSession();
 			Query query = session.createQuery("FROM SA1990 WHERE A1_COD = :codigo ");
 			query.setParameter("codigo", A1_COD);
-			Iterator it = query.iterate();
-			sa1 = (SA1990) it.next();
+			if (query.list().size() != 0) {
+				sa1 = (SA1990) query.list().get(0);
+			}
 		} catch (HibernateException e) {
 			e.printStackTrace();
 		}
